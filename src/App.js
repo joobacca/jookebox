69a90e2a72bad26ec4175a8991ff61e7c12e1dd1
Joo Han Kim <jk@von-der-see.de>
2020-03-12 19:36:49 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@material-ui/core";
+import io from 'socket.io-client';
+
+const SocketContext = React.createContext();
 
 function App() {
+  const socket = io('http://localhost:5000');
+  console.log(window.location.pathname);
+  socket.on('connect', args => {
+    socket.emit('joinRoom', window.location.pathname);
+    console.log(args);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button variant="contained" color="primary">
+            hi
+          </Button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </SocketContext.Provider>
   );
 }
 
